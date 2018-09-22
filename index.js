79a91771190c2dfdc93e4e7c2efb27f86b51cd93
Vincent Occhiogrosso <vocchiogrosso@users.noreply.github.com>
2018-09-22 13:42:02 -0400

    #1
    + Final Version (Reuploaded)

---
+++ index.js
@@ -0,0 +1,9 @@
+function mainF(){
+ $("#MainID").css("position","absolute");
+ $("#MainID").css("top","0px");
+ $("#MainID").css("left","0px");
+ $("#MainID").css("height",$(window).height());
+ $("#MainID").css("width",$(window).width());
+ $("#MainID").css("backgroundColor","gray");
+ console.log("A");
+}
